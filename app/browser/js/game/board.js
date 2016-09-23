995c8790a71bdf4e19fdee86bd60c49609f6870d
Kimber Lynch <kmlynch714@gmail.com>
2016-09-22 20:50:41 -0400

    cleanup board constructors

--- app/browser/js/game/board.js
+++ app/browser/js/game/board.js
@@ -1,103 +1,79 @@
-function Board() {
-    this.board = [];
+class Board {
+  constructor() { 
+  	this.board = []; 
+  }
+
+  drawBoard() {
+  	for (let y = 0; y < 6; y++) {
+      if (!this.board[y]) this.board[y] = [];
+      for (let x = 0; x < 6; x++) {
+          this.board[y].push(new Space(x, y, this.board));
+      }
+  	}
+  	return this.board;
+	}
 }
 
-Board.prototype.drawBoard = function () {
-    for (var y = 0; y < 6; y++) {
-        if (!this.board[y]) this.board[y] = [];
-        for (var x = 0; x < 6; x++) {
-            this.board[y].push(new Space(x, y, this.board));
-        }
-    }
-    return this.board;
-}
-
-function Space(x, y, board) {
+class Space {
+	constructor(x, y, board) {
     this.x = x;
     this.y = y;
-    this.points = [null, null, null, null, null, null, null, null];
-
-
-    for (var i = 0; i < 8; i++) {
-        let corresponding;
-
-        if (i < 2) { //top
-            corresponding = i === 0 ? 5 : 4; // 0 -> 5 & 1 -> 4
-            if (y === 0) this.points[i] = new Point([{
-                x: x,
-                y: y,
-                i: i
-            }]);
-            else {
-                this.points[i] = board[y - 1][x].points[corresponding];
-            }
-        } else if (i < 4) { //right
-            corresponding = i === 2 ? 7 : 6;
-            if (x === 5) this.points[i] = new Point([{
-                x: x,
-                y: y,
-                i: i
-            }]);
-            else {
-                this.points[i] = new Point([{
-                    x: x,
-                    y: y,
-                    i: i
-                }, {
-                    x: x + 1,
-                    y: y,
-                    i: corresponding
-                }]);
-            }
-        } else if (i < 6) { //bottom
-            corresponding = i === 4 ? 1 : 0;
-            if (y === 5) this.points[i] = new Point([{
-                x: x,
-                y: y,
-                i: i
-            }]);
-            else {
-                this.points[i] = new Point([{
-                    x: x,
-                    y: y,
-                    i: i
-                }, {
-                    x: x,
-                    y: y + 1,
-                    i: corresponding
-                }]);
-            }
-        } else { //left
-            corresponding = i === 6 ? 3 : 2; // 6 -> 3 & 7 -> 2
-            if (x === 0) this.points[i] = new Point([{
-                x: x,
-                y: y,
-                i: i
-            }]);
-            else {
-                this.points[i] = board[y][x - 1].points[corresponding];
-            }
-        }
-    }
+    this.points = generatePoints(x,y,board);
+  }
 }
 
+class Point{
+	constructor(spaces) {
+		if(spaces.length > 1) this.edge = false;
+		else this.edge = true;
+	  
+	  this.spaceId = 'space' + spaces[0].y + spaces[0].x + spaces[0].i;
+	  this.travelled = false;
+	  this.spaces = spaces;
+	}
+}
 
-// edge = boolean
-function Point(space) {
-    if (space[0].y === 0) {
-        if (space[0].i === 0 || space[0].i === 1) this.edge = true;
-        else this.edge = false;
-    } else if (space[0].y === 5) {
-        if (space[0].i === 4 || space[0].i === 5) this.edge = true;
-        else this.edge = false;
-    } else if (space[0].x === 0) {
-        if (space[0].i === 6 || space[0].i === 7) this.edge = true;
-        else this.edge = false;
-    } else if (space[0].x === 5) {
-        if (space[0].i === 2 || space[0].i === 3) this.edge = true;
-        else this.edge = false;
+function generatePoints(x,y,board){
+	/* 
+		When generating the points for a space we will do one of two actions:
+		1. Create a reference to an existing point
+		2. Create a new point
+		We decide reference or new point based on the space's position in the board
+	*/
+
+	let array = new Array(8);
+
+	for (let i = 0; i < 8; i++) {
+    let sharedPoint;
+
+    //TOP POINTS
+    if (i < 2) { 
+    	sharedPoint = (i === 0) ? 5 : 4; 
+      if (y === 0) { array[i] = new Point([{ x: x,y: y, i: i }]); }
+      else { array[i] = board[y - 1][x].points[sharedPoint]; }   
+    } 
+		
+		//RIGHT SIDE POINTS
+    else if (i < 4) { 
+      sharedPoint = i === 2 ? 7 : 6; 
+      if (x === 5) { array[i] = new Point([{ x: x, y: y, i: i }]); }
+      else { array[i] = new Point([{ x: x, y: y, i: i }, { x: x + 1, y: y, i: sharedPoint}]); } 
+    } 
+
+    //BOTTOM POINTS
+    else if (i < 6) {
+      sharedPoint = i === 4 ? 1 : 0;
+      if (y === 5) { array[i] = new Point([{ x: x, y: y, i: i }]); }
+      else { array[i] = new Point([{ x: x, y: y, i: i }, { x: x, y: y + 1, i: sharedPoint }]); }
+    } 
+
+    //LEFT POINTS
+    else {
+      sharedPoint = i === 6 ? 3 : 2; 
+      if (x === 0) { array[i] = new Point([{ x: x, y: y, i: i }]); }
+      else { array[i] = board[y][x - 1].points[sharedPoint]; }
     }
-    this.spaceId = 'space' + space[0].y + space[0].x + space[0].i;
-    this.travelled = false;
-    this.spaces = space;
+  }
+
+  return array;
 }
